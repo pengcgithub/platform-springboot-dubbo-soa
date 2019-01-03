b2f2386c2f0da30f89959558da568d5cc954ddad
ssj <pengcheng3211@gmail.com>
2019-01-04 00:25:07 +0800

    注释dubbo启动类

--- platform-user-provider/src/main/java/com/platform/soa/user/PlatformUserProviderApplication.java
+++ platform-user-provider/src/main/java/com/platform/soa/user/PlatformUserProviderApplication.java
@@ -1,19 +1,23 @@
 package com.platform.soa.user;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//@EnableTransactionManagement
-//@ComponentScan(basePackages = "com.platform.soa")
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.platform.soa.user.service")
+@EnableJpaRepositories(basePackages = "com.platform.soa.user.dao")
+@EntityScan(basePackages = "com.platform.soa.user.domain")
 @SpringBootApplication
 public class PlatformUserProviderApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(PlatformUserProviderApplication.class, args);
         new SpringApplicationBuilder(PlatformUserProviderApplication.class)
-                .web(false) // 非 Web 应用
+//                .web(false)
                 .run(args);
     }
 
