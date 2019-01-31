fd5b35e7dcef08b0591c219b810d12aa5b49dc93
ssj <pengcheng3211@gmail.com>
2019-01-31 22:47:12 +0800

    移除无效注释

--- platform-user/platform-user-provider/src/main/java/com/platform/soa/user/PlatformUserProviderApplication.java
+++ platform-user/platform-user-provider/src/main/java/com/platform/soa/user/PlatformUserProviderApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
